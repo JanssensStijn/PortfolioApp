95d01f5c93b74e48fbb463f1028edcb98e57ced1
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-10-02 20:12:19 +0200

    possible public_url fix

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -26,7 +26,8 @@ type Config = {
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrlString = process.env.PUBLIC_URL || window.location.origin;
+    const publicUrl = new URL(publicUrlString, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
