3b121dc30014d57e034cf3733c97698489f1ade8
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-10-02 19:31:11 +0200

    register serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import './color.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // Import the service worker registration
+
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -18,7 +20,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+// Register the service worker
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
