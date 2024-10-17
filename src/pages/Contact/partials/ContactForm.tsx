3ca15d573fa2acf5bf20989dee524464dc3266bb
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-10-17 21:40:30 +0200

    offline message for form and login

--- src/pages/Contact/partials/ContactForm.tsx
+++ src/pages/Contact/partials/ContactForm.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, FormControl, FormHelperText, InputLabel, MenuItem, Paper, Select, TextField } from "@mui/material";
-import { FC, useState } from "react";
+import { Box, Button, FormControl, FormHelperText, InputLabel, MenuItem, Paper, Select, TextField, Typography} from "@mui/material";
+import { FC, useEffect, useState } from "react";
 import { addNewMessage } from "../../../firebase/MessageChanges";
 import SendIcon from '@mui/icons-material/Send';
 import validator from 'validator';
@@ -36,6 +36,21 @@ export const ContactForm: FC = () => {
         setErrors(newErrors);
         return Object.keys(newErrors).length === 0;
     };
+
+    const [isOnline, setIsOnline] = useState(window.navigator.onLine);
+
+    useEffect(() => {
+      const handleOnline = () => setIsOnline(true);
+      const handleOffline = () => setIsOnline(false);
+  
+      window.addEventListener('online', handleOnline);
+      window.addEventListener('offline', handleOffline);
+  
+      return () => {
+        window.removeEventListener('online', handleOnline);
+        window.removeEventListener('offline', handleOffline);
+      };
+    }, [isOnline]);
     
     const onSend = async () => {
         if (validateForm()) {
@@ -122,120 +137,131 @@ export const ContactForm: FC = () => {
     };
 
     return (  
-        <Box display="flex" flexDirection="column" height="50vh" alignItems="center" margin={5}>
-            {success && <Box sx={{border: 1, borderColor: 'success.main', borderRadius: 3,marginBottom:2, padding: 2, backgroundColor: 'success.light'
-            }}><p>Message send successfully!</p></Box>}
-            {failure && <Box sx={{border: 1, borderColor: '#d32f2f', borderRadius: 3,marginBottom:2, padding: 2, backgroundColor: '#d32f2f'
-            }}><p>Failed to send message!</p></Box>}
-            <Paper elevation={3} sx={{padding:'16px'}}>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    
-                    <TextField
-                        id="firstname"
-                        label="Firstname"
-                        fullWidth
-                        value={contactFormInput.firstnameOfContact}
-                        onChange={(e) => onFirstnameChange(e.target.value)}
-                        required
-                        error={Boolean(errors.firstnameOfContact)}
-                        helperText={errors.firstnameOfContact}
-                    />
-                    <TextField
-                        id="lastname"
-                        label="Lastname"
-                        fullWidth
-                        value={contactFormInput.lastnameOfContact}
-                        onChange={(e) => onLastnameChange(e.target.value)}
-                        required
-                        error={Boolean(errors.lastnameOfContact)}
-                        helperText={errors.lastnameOfContact}
-                    />
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    <TextField
-                        id="job title"
-                        label="job title"
-                        fullWidth
-                        value={contactFormInput.jobtitleOfContact}
-                        onChange={(e) => onJobtitleChange(e.target.value)}
-                        error={Boolean(errors.jobtitleOfContact)}
-                        helperText={errors.jobtitleOfContact}
-                    />
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    <TextField
-                        id="company"
-                        label="company"
-                        fullWidth
-                        value={contactFormInput.companyOfContact}
-                        onChange={(e) => onCompanyChange(e.target.value)}
-                        error={Boolean(errors.companyOfContact)}
-                        helperText={errors.companyOfContact}
-                    />
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    <TextField
-                        id="mail"
-                        label="email-address"
-                        fullWidth
-                        value={contactFormInput.mailOfContact}
-                        onChange={(e) => onMailChange(e.target.value)}
-                        required
-                        error={Boolean(errors.mailOfContact)}
-                        helperText={errors.mailOfContact}
-                    />
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    <FormControl variant="outlined" fullWidth>
-                        <InputLabel 
-                        id="questionSelect"
-                        error={Boolean(errors.question)}
-                        >Your question</InputLabel>
-                        <Select fullWidth
-                            labelId="questionSelect"
-                            id="questionSelect"
-                            label="Your question"
-                            value={contactFormInput.question} 
-                            onChange={(e) => onQuestionChange(e.target.value)} 
+        <>
+            {isOnline && <Box display="flex" flexDirection="column" height="50vh" alignItems="center" margin={5}>
+                {success && <Box sx={{border: 1, borderColor: 'success.main', borderRadius: 3,marginBottom:2, padding: 2, backgroundColor: 'success.light'
+                }}><p>Message send successfully!</p></Box>}
+                {failure && <Box sx={{border: 1, borderColor: '#d32f2f', borderRadius: 3,marginBottom:2, padding: 2, backgroundColor: '#d32f2f'
+                }}><p>Failed to send message!</p></Box>}
+                <Paper elevation={3} sx={{padding:'16px'}}>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        
+                        <TextField
+                            id="firstname"
+                            label="Firstname"
+                            fullWidth
+                            value={contactFormInput.firstnameOfContact}
+                            onChange={(e) => onFirstnameChange(e.target.value)}
                             required
+                            error={Boolean(errors.firstnameOfContact)}
+                            helperText={errors.firstnameOfContact}
+                        />
+                        <TextField
+                            id="lastname"
+                            label="Lastname"
+                            fullWidth
+                            value={contactFormInput.lastnameOfContact}
+                            onChange={(e) => onLastnameChange(e.target.value)}
+                            required
+                            error={Boolean(errors.lastnameOfContact)}
+                            helperText={errors.lastnameOfContact}
+                        />
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        <TextField
+                            id="job title"
+                            label="job title"
+                            fullWidth
+                            value={contactFormInput.jobtitleOfContact}
+                            onChange={(e) => onJobtitleChange(e.target.value)}
+                            error={Boolean(errors.jobtitleOfContact)}
+                            helperText={errors.jobtitleOfContact}
+                        />
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        <TextField
+                            id="company"
+                            label="company"
+                            fullWidth
+                            value={contactFormInput.companyOfContact}
+                            onChange={(e) => onCompanyChange(e.target.value)}
+                            error={Boolean(errors.companyOfContact)}
+                            helperText={errors.companyOfContact}
+                        />
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        <TextField
+                            id="mail"
+                            label="email-address"
+                            fullWidth
+                            value={contactFormInput.mailOfContact}
+                            onChange={(e) => onMailChange(e.target.value)}
+                            required
+                            error={Boolean(errors.mailOfContact)}
+                            helperText={errors.mailOfContact}
+                        />
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        <FormControl variant="outlined" fullWidth>
+                            <InputLabel 
+                            id="questionSelect"
                             error={Boolean(errors.question)}
+                            >Your question</InputLabel>
+                            <Select fullWidth
+                                labelId="questionSelect"
+                                id="questionSelect"
+                                label="Your question"
+                                value={contactFormInput.question} 
+                                onChange={(e) => onQuestionChange(e.target.value)} 
+                                required
+                                error={Boolean(errors.question)}
 
-                        >
-                            <MenuItem value="General">General</MenuItem>
-                            <MenuItem value="Job offer">Job Offer</MenuItem>
-                        </Select>
-                        {errors.question && <FormHelperText sx={{ color: '#d32f2f' }}>{errors.question}</FormHelperText>}
-                    </FormControl>
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
-                    <TextField
-                        label="Message"
-                        fullWidth
-                        value={contactFormInput.message}
-                        multiline
-                        onChange={(e) => onMessageChange(e.target.value)}
-                        required
-                        error={Boolean(errors.message)}
-                        helperText={errors.message}
-                    />
-                </Box>
-                <Box display="flex" maxWidth={500} width={1} columnGap={2}>
+                            >
+                                <MenuItem value="General">General</MenuItem>
+                                <MenuItem value="Job offer">Job Offer</MenuItem>
+                            </Select>
+                            {errors.question && <FormHelperText sx={{ color: '#d32f2f' }}>{errors.question}</FormHelperText>}
+                        </FormControl>
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2} paddingBottom={2}>
+                        <TextField
+                            label="Message"
+                            fullWidth
+                            value={contactFormInput.message}
+                            multiline
+                            onChange={(e) => onMessageChange(e.target.value)}
+                            required
+                            error={Boolean(errors.message)}
+                            helperText={errors.message}
+                        />
+                    </Box>
+                    <Box display="flex" maxWidth={500} width={1} columnGap={2}>
 
-                    <ReCAPTCHA 
-                        sitekey= {process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
-                        onChange={() => setCaptchaSuccess(true)} 
-                        onExpired={() => setCaptchaSuccess(false)}
-                        onError={() => setCaptchaSuccess(false)}
-                    />
+                        <ReCAPTCHA 
+                            sitekey= {process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
+                            onChange={() => setCaptchaSuccess(true)} 
+                            onExpired={() => setCaptchaSuccess(false)}
+                            onError={() => setCaptchaSuccess(false)}
+                        />
 
-                    {captchaSuccess && <Button variant="contained"
-                        endIcon={<SendIcon/>} 
-                        onClick={onSend}
-                        fullWidth>
-                        SEND
-                    </Button>}
+                        {captchaSuccess && <Button variant="contained"
+                            endIcon={<SendIcon/>} 
+                            onClick={onSend}
+                            fullWidth>
+                            SEND
+                        </Button>}
+                    </Box>
+                </Paper>
+            </Box>
+            }
+            {!isOnline && <Box display="flex" flex={1} flexDirection="column" textAlign="center" alignItems="center">
+                <Typography variant="h4" sx={{marginBottom:"20px"}}>Your currently offline</Typography>
+                <Typography variant="body1">If you would like to send in a contactform, please make sure you have a internet connection.</Typography>
+                <Button variant="contained" sx={{marginTop:"20px"}} color="primary" onClick={() => window.location.reload()}>
+                    Refresh & Try again
+                </Button>
                 </Box>
-            </Paper>
-        </Box>
+            }
+        </>
     );
 }
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -31,61 +31,88 @@ export const LoginPage: FC = () => {
     }
   };
 
-  return (
-    <Box display="flex" flex={1} flexDirection="column" textAlign="center" alignItems="center">
-      <Typography variant="h3" sx={{color: 'var(--accentColor)', marginBottom:"20px"}}>Login</Typography>
-      <Typography variant="h4" sx={{marginBottom:"20px"}}>For administrators only</Typography>
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        width={1}
-      >
-        <Paper elevation={3}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            maxWidth="500px"
-            width={1}
-            padding={2}
-            gap={2}
-          >
-            <TextField
-              variant="outlined"
-              label="Email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              label="Password"
-              value={password}
-              type="password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <Box display="flex" gap={2}>
-              <ReCAPTCHA
-                sitekey= {process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
-                onChange={() => setCaptchaSuccess(true)} 
-                onExpired={() => setCaptchaSuccess(false)}
-                onError={() => setCaptchaSuccess(false)}
-              />
+  const [isOnline, setIsOnline] = useState(window.navigator.onLine);
 
-              {captchaSuccess && 
-              <Button
-                startIcon={<Login/>}
-                variant="contained"
-                color="primary"
-                fullWidth
-                onClick={onSignin}
-              >
-                Signin
-              </Button>}
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, [isOnline]);
+
+  return (
+    <>
+      {isOnline && <Box display="flex" flex={1} flexDirection="column" textAlign="center" alignItems="center">
+        <Typography variant="h3" sx={{color: 'var(--accentColor)', marginBottom:"20px"}}>Login</Typography>
+        <Typography variant="h4" sx={{marginBottom:"20px"}}>For administrators only</Typography>
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          width={1}
+        >
+          <Paper elevation={3}>
+            <Box
+              display="flex"
+              flexDirection="column"
+              maxWidth="500px"
+              width={1}
+              padding={2}
+              gap={2}
+            >
+              <TextField
+                variant="outlined"
+                label="Email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <TextField
+                variant="outlined"
+                label="Password"
+                value={password}
+                type="password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <Box display="flex" gap={2}>
+                <ReCAPTCHA
+                  sitekey= {process.env.REACT_APP_RECAPTCHA_SITE_KEY as string}
+                  onChange={() => setCaptchaSuccess(true)} 
+                  onExpired={() => setCaptchaSuccess(false)}
+                  onError={() => setCaptchaSuccess(false)}
+                />
+
+                {captchaSuccess && 
+                <Button
+                  startIcon={<Login/>}
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={onSignin}
+                >
+                  Signin
+                </Button>}
+              </Box>
             </Box>
-          </Box>
-        </Paper>
+          </Paper>
+        </Box>
       </Box>
-    </Box>
+      }
+      {!isOnline && <Box display="flex" flex={1} flexDirection="column" textAlign="center" alignItems="center">
+          <Typography variant="h3" sx={{color: 'var(--accentColor)', marginBottom:"20px"}}>Login</Typography>
+          <Typography variant="h4" sx={{marginBottom:"20px"}}>Your currently offline</Typography>
+          <Typography variant="body1">If you would like to login, please make sure you have a internet connection.</Typography>
+          <Button variant="contained" sx={{marginTop:"20px"}} color="primary" onClick={() => window.location.reload()}>
+            Refresh & Try again
+          </Button>
+        </Box>
+      }
+    </>
   );
 };
